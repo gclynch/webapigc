4fdc77b6c49064d7c52487cee7c931368e5223fe
Gary Clynch <gary.clynch@tudublin.ie>
2020-11-18 11:45:54 +0000

    first commit

---
+++ HelloWorldAPI/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HelloWorldAPI.Controllers
+{
+    [ApiController]
+    [Route("api/hello")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("{name}")]
+        public IActionResult SayHello(String name)
+        {
+            return Ok(new Greeting() { Name = name, Message = "hello " });
+        }
+    }
+
+    public class Greeting
+    {
+        public String Name { get; set; }
+        public String Message { get; set; }
+    }
+}
